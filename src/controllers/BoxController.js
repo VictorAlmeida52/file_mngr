e7c592aaffd44f1810e9d8c1e8ece508f46469a7
Victor Almeida <victor.almeida@selens.com.br>
2019-09-05 15:21:22 -0300

    findOne

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -1,16 +1,20 @@
-const Box = require('../models/Box');
+const Box = require("../models/Box");
 
 class BoxController {
   async store(req, res) {
-  
-    const box = await Box.create(req.body)
+    const box1 = await Box.findOne({ title: req.body.title });
 
-    return res.json(box);
+    if (box1) {
+      return res.json(box1);
+    } else {
+      const box = await Box.create(req.body);
+      return res.json(box);
+    }
   }
 
   async show(req, res) {
     const box = await Box.findById(req.params.id).populate({
-      path: 'files',
+      path: "files",
       options: { sorty: { createdAt: -1 } }
     });
 
